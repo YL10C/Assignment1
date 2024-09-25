a5820a06eda33ef245158ebd90495a30494c214b
Yilin Chen <yuyuxiangqian@gmail.com>
2024-09-25 21:51:47 +0100

    Correction to questions 5-7

--- proj1.r
+++ proj1.r
@@ -1,8 +1,9 @@
-## Yilin Chen s2134652;
+## Yilin Chen s2134652; Xuanyu Hu s2676244;
 ##
 
 ## comment out of submitted
-##setwd("D:/PG-CAM/Statistical Programming") # nolint
+## setwd("D:/PG-CAM/Statistical Programming") # nolint
+setwd("D:\\OneDrive\\文档\\Yilin\\Edinburgh\\Statistical Programming\\Assignment1") # nolint
 a <- scan(
   file = "4300-0.txt", # file name
   what = "character", # file are read as character vectors
@@ -15,8 +16,8 @@ a <- gsub("_(", "", a, fixed = TRUE) ## replace "_(" by ""
 
 
 split_punct <- function(a) {
-  # index of elements containing punctuation
-  ia <- grep("[[:punct:]]", a)
+  # index of elements containing punctuation at the end
+  ia <- grep("[[:punct:]]$", a)
   # new vector stored words and puncts
   res <- rep("", length(ia) + length(a))
   ## where should punct go in res
@@ -34,51 +35,41 @@ split_punct <- function(a) {
 res <- split_punct(a)
 
 
-##Xuanyu Hu s2676244
-
-#replace the specified punctuation mark with a separate form
-res <- gsub("([,.!?;:])", " \\1", res)
-
-#Converts words in text to lower case
-lower_a <- tolower(a)
+# Converts words in text to lower case
+lower_a <- tolower(res)
 unique_words <- unique(lower_a)
 
-#Use match to find the index vector
+# Use match to find the index vector
 index_vector <- match(lower_a, unique_words)
 
-#Count the number of times each unique word appears in the text
+
+# Count the number of times each unique word appears in the text
 word_counts <- tabulate(index_vector, nbins = length(unique_words))
 
-#Determine the threshold of occurrence times
+
+# Determine the threshold of occurrence times
 word_table <- data.frame(Word = unique_words, Count = word_counts)
 sorted_word_table <- word_table[order(-word_table$Count), ]
-threshold <- sorted_word_table[1000, "Count"] 
 
-#Create a vector b containing the most common m words
+
+# Create a vector b containing the most common m words
 m <- 1000
 b <- sorted_word_table[1:m, "Word"]
 
-print(threshold)
-print(b)
 
 # Set mlag value
-mlag <- 4  # Maximum lag considered
+mlag <- 4
 
 # Match the lower case Ulysses text to the common words vector
-lower_a <- tolower(a)  # Assume 'a' contains the original text vector
-index_vector <- match(lower_a, b)  # Using the previously obtained most common words
+index_vector <- match(lower_a, b)
 
 # Create the matrix M
-n <- length(index_vector)  # Length of the index vector
-M <- matrix(NA, nrow = n - mlag, ncol = mlag + 1)  # Initialize the matrix
+n <- length(index_vector) # Length of the index vector
+M <- matrix(NA, nrow = n - mlag, ncol = mlag + 1) # Initialize the matrix
 
-for (i in 0:mlag) {
-  M[, i + 1] <- index_vector[(1 + i):(n - mlag + i)]  # Fill in the shifted indices
+for (i in 1:(mlag + 1)) {
+  ## Assignment to all rows in column i of matrix M
+  M[, i] <- index_vector[i:(n - mlag + i - 1)]
 }
 
-print(M)
-
-
-
-
-
+M
--- test.r
+++ test.r
@@ -1,14 +1,13 @@
-a <- c("_(", "acda,", "bsdfgrgw.", "c")
+index_vector <- 1:10
+mlag <- 4
 
-ia <- grep("[[:punct:]]", a) # index of elements containing punctuation
+# Create the matrix M
+n <- length(index_vector) # Length of the index vector
+M <- matrix(NA, nrow = n - mlag, ncol = mlag + 1) # Initialize the matrix
 
-ia
+for (i in 1:(mlag + 1)) {
+  ## Assignment to all rows in column i of matrix M
+  M[, i] <- index_vector[i:(n - mlag + i - 1)]
+}
 
-res <- rep("", length(ia) + length(a)) # new vector stored words and puncts
-
-iia <- ia + 1:length(ia) ## where should punct go in res
-
-res[iia] <- substr(a[ia], nchar(a[ia]), nchar(a[ia]))
-res[iia - 1] <- substr(a[ia], 1, nchar(a[ia]) - 1)
-res[-c(iia, iia - 1)] <- a[-ia]
-res
+M
