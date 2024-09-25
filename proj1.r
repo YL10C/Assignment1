a12ccdc71ee0124d45b6559591ab11f92d26c9be
Xuanyu990726 <s2676244@ed.ac.uk>
2024-09-25 19:37:27 +0100

    huxuanyu-change1

--- proj1.r
+++ proj1.r
@@ -2,10 +2,9 @@
 ##
 
 ## comment out of submitted
-#setwd("D:/OneDrive/文档/Yilin/Edinburgh/Statistical Programming/Assignment1") # nolint
-
+##setwd("D:/PG-CAM/Statistical Programming") # nolint
 a <- scan(
-  "4300-0.txt", # file name
+  file = "4300-0.txt", # file name
   what = "character", # file are read as character vectors
   skip = 73, # skip the first 73 lines of the file
   nlines = 32858 - 73, # specifies the number of rows to read
@@ -33,3 +32,53 @@ split_punct <- function(a) {
 }
 
 res <- split_punct(a)
+
+
+##Xuanyu Hu s2676244
+
+#replace the specified punctuation mark with a separate form
+res <- gsub("([,.!?;:])", " \\1", res)
+
+#Converts words in text to lower case
+lower_a <- tolower(a)
+unique_words <- unique(lower_a)
+
+#Use match to find the index vector
+index_vector <- match(lower_a, unique_words)
+
+#Count the number of times each unique word appears in the text
+word_counts <- tabulate(index_vector, nbins = length(unique_words))
+
+#Determine the threshold of occurrence times
+word_table <- data.frame(Word = unique_words, Count = word_counts)
+sorted_word_table <- word_table[order(-word_table$Count), ]
+threshold <- sorted_word_table[1000, "Count"] 
+
+#Create a vector b containing the most common m words
+m <- 1000
+b <- sorted_word_table[1:m, "Word"]
+
+print(threshold)
+print(b)
+
+# Set mlag value
+mlag <- 4  # Maximum lag considered
+
+# Match the lower case Ulysses text to the common words vector
+lower_a <- tolower(a)  # Assume 'a' contains the original text vector
+index_vector <- match(lower_a, b)  # Using the previously obtained most common words
+
+# Create the matrix M
+n <- length(index_vector)  # Length of the index vector
+M <- matrix(NA, nrow = n - mlag, ncol = mlag + 1)  # Initialize the matrix
+
+for (i in 0:mlag) {
+  M[, i + 1] <- index_vector[(1 + i):(n - mlag + i)]  # Fill in the shifted indices
+}
+
+print(M)
+
+
+
+
+
